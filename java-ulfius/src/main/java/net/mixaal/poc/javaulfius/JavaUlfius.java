0532c52e13d01de6361842ffe86dd39fc41d1bce
Michal Conos <michal.conos@oracle.com>
2018-10-11 15:49:27 +0200

    ulfius native image bindings

---
+++ java-ulfius/src/main/java/net/mixaal/poc/javaulfius/JavaUlfius.java
@@ -0,0 +1,185 @@
+package net.mixaal.poc.javaulfius;
+
+import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.nativeimage.StackValue;
+import org.graalvm.nativeimage.c.CContext;
+import org.graalvm.nativeimage.c.constant.CConstant;
+import org.graalvm.nativeimage.c.function.*;
+import org.graalvm.nativeimage.c.struct.CField;
+import org.graalvm.nativeimage.c.struct.CStruct;
+import org.graalvm.nativeimage.c.type.CCharPointer;
+import org.graalvm.nativeimage.c.type.CTypeConversion;
+import org.graalvm.word.ComparableWord;
+import org.graalvm.word.Pointer;
+import org.graalvm.word.PointerBase;
+import org.graalvm.word.WordFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class JavaUlfius {
+
+    @CContext(Ulfius.UDirectives.class)
+    public static final class Ulfius  {
+        @CStruct(value = "_u_instance", addStructKeyword = true)
+        interface Instance extends PointerBase {
+
+        }
+
+        @CStruct(value = "_u_response", addStructKeyword = true)
+        interface Response extends PointerBase {
+
+        }
+
+        @CStruct(value = "_u_request", addStructKeyword = true)
+        interface Request extends PointerBase {
+
+        }
+
+        /**
+         * int ulfius_init_instance(
+         *      struct _u_instance * u_instance,
+         *      unsigned int port,
+         *      struct sockaddr_in * bind_address,
+         *      const char * default_auth_realm
+         *      );
+         */
+        @CFunction
+        static native int ulfius_init_instance(Instance instance, int port, Pointer bind_addr_in, Pointer realm);
+
+
+        @CFunction
+        static native int ulfius_stop_framework(Instance instance);
+
+        @CFunction
+        static native int ulfius_start_framework(Instance instance);
+
+        @CFunction
+        static native int ulfius_clean_instance(Instance instance);
+
+        @CFunction
+        static native int ulfius_add_endpoint_by_val(
+                CCharPointer httpMethod,
+                CCharPointer prefix,
+                CCharPointer format,
+                int priority,
+                Handler handler,
+                Pointer userData
+        );
+
+        interface Handler extends CFunctionPointer {
+
+            @InvokeCFunctionPointer
+            int invoke(IsolateThread thread, Request request, Response response, Pointer userData);
+        }
+
+        /**
+         * int ulfius_add_endpoint_by_val(struct _u_instance * u_instance,
+         const char * http_method,
+         const char * url_prefix,
+         const char * url_format,
+         unsigned int priority,
+         int (* callback_function)(const struct _u_request * request, // Input parameters (set by the framework)
+         struct _u_response * response,     // Output parameters (set by the user)
+         void * user_data),
+         void * user_data);
+         *
+         */
+
+        @CConstant
+        static final native int U_OK();
+
+        @CConstant
+        static final native int U_CALLBACK_CONTINUE();
+
+        public  static class UDirectives implements CContext.Directives {
+            @Override
+            public List<String> getHeaderFiles() {
+                return Arrays.asList("<ulfius.h>");
+            }
+
+            @Override
+            public List<String> getLibraries() {
+                return Arrays.asList("ulfius");
+            }
+        }
+    }
+
+    @CEntryPoint
+    private static int callback_hello_world(IsolateThread isolateThread, Ulfius.Request request, Ulfius.Response response, Pointer userData) {
+        System.out.println("hello");
+        return Ulfius.U_CALLBACK_CONTINUE();
+    }
+
+    private static final CEntryPointLiteral<Ulfius.Handler> handlerInstance =
+            CEntryPointLiteral.create(JavaUlfius.class,
+                    "callback_hello_world",
+                    IsolateThread.class,
+                    Ulfius.Request.class,
+                    Ulfius.Response.class,
+                    Pointer.class
+            );
+
+    public static void main(String []args) {
+        Ulfius.Instance instance  = StackValue.get(Ulfius.Instance.class);
+        int initPhaseResult = Ulfius.ulfius_init_instance(instance, 8080, WordFactory.nullPointer(), WordFactory.nullPointer());
+        System.out.printf("init result: %d\n", initPhaseResult);
+        Ulfius.ulfius_add_endpoint_by_val(
+                CTypeConversion.toCString("GET").get(),
+                CTypeConversion.toCString("/helloworld").get(),
+                WordFactory.nullPointer(),
+                0,
+                handlerInstance.getFunctionPointer(),
+                WordFactory.nullPointer()
+        );
+        int result = Ulfius.ulfius_start_framework(instance);
+        System.out.printf("U_OK()=%d result=%d\n", Ulfius.U_OK(), result);
+        Ulfius.ulfius_stop_framework(instance);
+        Ulfius.ulfius_clean_instance(instance);
+    }
+
+
+    /**
+     * /**
+     *  * Callback function for the web application on /helloworld url call
+     *
+    int callback_hello_world (const struct _u_request * request, struct _u_response * response, void * user_data) {
+        ulfius_set_string_body_response(response, 200, "Hello World!");
+        return U_CALLBACK_CONTINUE;
+    }
+
+    /**
+     *   main function
+     *
+    int main(void) {
+        struct _u_instance instance;
+
+        // Initialize instance with the port number
+        if (ulfius_init_instance(&instance, PORT, NULL, NULL) != U_OK) {
+            fprintf(stderr, "Error ulfius_init_instance, abort\n");
+            return(1);
+        }
+
+        // Endpoint list declaration
+        ulfius_add_endpoint_by_val(&instance, "GET", "/helloworld", NULL, 0, &callback_hello_world, NULL);
+
+        // Start the framework
+        if (ulfius_start_framework(&instance) == U_OK) {
+            printf("Start framework on port %d\n", instance.port);
+
+            // Wait for the user to press <enter> on the console to quit the application
+            getchar();
+        } else {
+            fprintf(stderr, "Error starting framework\n");
+        }
+        printf("End framework\n");
+
+        ulfius_stop_framework(&instance);
+        ulfius_clean_instance(&instance);
+
+        return 0;
+    }
+
+     */
+}
